40a19d7c66bd91c9aa798f3815027e48cbea3731
Evan Weaver <evan@cloudbur.st>
2008-01-20 08:09:36 +0000

    better benchmark

--- test/benchmark/benchmark_test.rb
+++ test/benchmark/benchmark_test.rb
@@ -7,21 +7,33 @@ class BenchmarkTest < Test::Unit::TestCase
 
   def setup
     @value = OpenStruct.new(:a => 1, :b => 2, :c => GenericClass)
+    @opts = [
+      ['127.0.0.1:43042', '127.0.0.1:43043'], 
+      {:namespace => "benchmark_namespace"}
+    ]
   end
 
   def test_original_speed
-    @cache = MemCache.new(['127.0.0.1:43042', '127.0.0.1:43043'])
-    assert_faster(0.01) do
-      @cache.set 'test_speed', @value
-      @cache.get 'test_speed'
+    @cache = MemCache.new(*@opts)
+    assert_faster(0.02) do
+      @cache.set 'key1', @value
+      @cache.get 'key1'
+      @cache.set 'key2', @value
+      @cache.set 'key3', @value
+      @cache.get 'key2'
+      @cache.get 'key3'
     end
   end
   
   def test_new_speed
-    @cache = Memcached.new(['127.0.0.1:43042', '127.0.0.1:43043'])
-    assert_faster(0.001) do
-      @cache.set 'test_speed', @value
-      @cache.get 'test_speed'
+    @cache = Memcached.new(*@opts)
+    assert_faster(0.005) do
+      @cache.set 'key1', @value
+      @cache.get 'key1'
+      @cache.set 'key2', @value
+      @cache.set 'key3', @value
+      @cache.get 'key2'
+      @cache.get 'key3'
     end  
   end
   
