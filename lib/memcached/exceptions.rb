c9820e849a64dbaae62a9b51b2693368e197f560
Evan Weaver <evan@cloudbur.st>
2008-01-20 02:54:05 +0000

    forgot exceptions file

---
+++ lib/memcached/exceptions.rb
@@ -0,0 +1,27 @@
+
+class Memcached
+
+  class Error < RuntimeError
+  end
+  
+  class NotImplemented < StandardError
+  end
+
+  class << self
+    private
+    def camelize(string)
+      string.downcase.split(' ').map {|s| s.capitalize}.join
+    end   
+  end
+  
+  @@exceptions = []
+  @@empty_struct = Libmemcached::MemcachedSt.new
+  # Libmemcached.memcached_create(@@empty_struct)
+  
+  Libmemcached::MEMCACHED_MAXIMUM_RETURN.times do |exception_index|    
+    description = Libmemcached.memcached_strerror(@@empty_struct, exception_index)
+    exception_class = eval("class #{camelize(description)} < Error; self; end")
+    @@exceptions << exception_class
+  end
+  
+end
