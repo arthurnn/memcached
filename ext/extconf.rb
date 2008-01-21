dd2325cd7c424c5913769808e55c871287092e8a
Evan Weaver <evan@cloudbur.st>
2008-01-21 07:37:04 +0000

    i thought mkmf did that automatically... oh well

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -12,6 +12,13 @@ if ENV['DEBUG']
 end
 
 dir_config 'libmemcached'
-find_library 'memcached', 'memcached_server_add'
-find_header 'libmemcached/memcached.h', *ENV['INCLUDE_PATH'].to_s.split(":")
+
+# XXX There's probably a better way to do this
+unless find_library 'memcached', 'memcached_server_add', *ENV['LD_LIBRARY_PATH'].to_s.split(":")
+  raise "shared library 'libmemcached' not found"
+end
+unless find_header 'libmemcached/memcached.h', *ENV['INCLUDE_PATH'].to_s.split(":")
+  raise "header file 'libmemcached/memcached.h' not  found"
+end
+
 create_makefile 'libmemcached'
