472384b3587dfa3175283a7d1b0e5ff3fd0e6c41
Evan Weaver <evan@cloudbur.st>
2008-01-24 20:53:20 +0000

    formatting

--- lib/memcached.rb
+++ lib/memcached.rb
@@ -10,11 +10,11 @@ The generated SWIG module for accessing libmemcached's C API.
 
 Includes the full set of libmemcached static methods (as defined in <tt>$INCLUDE_PATH/libmemcached/memcached.h</tt>), and classes for the available structs:
 
-* Libmemcached::MemcachedResultSt
-* Libmemcached::MemcachedServerSt
-* Libmemcached::MemcachedSt
-* Libmemcached::MemcachedStatSt
-* Libmemcached::MemcachedStringSt
+* <b>Libmemcached::MemcachedResultSt</b>
+* <b>Libmemcached::MemcachedServerSt</b>
+* <b>Libmemcached::MemcachedSt</b>
+* <b>Libmemcached::MemcachedStatSt</b>
+* <b>Libmemcached::MemcachedStringSt</b>
 
 A number of SWIG typemaps and C helper methods are also defined in <tt>ext/libmemcached.i</tt>.
 
