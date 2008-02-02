ec0f272b1e00154659b8608a6df21ff01840cd6a
Evan Weaver <evan@cloudbur.st>
2008-02-02 04:19:33 +0000

    Cleaner valgrind script.

--- test/benchmark/valgrind.rb
+++ test/benchmark/valgrind.rb
@@ -4,35 +4,44 @@ $LOAD_PATH << "#{HERE}/../../lib/"
 
 require 'memcached'
 
-@value = []
-@marshalled = Marshal.dump(@value)
-@opts = [
-  ['127.0.0.1:43042', '127.0.0.1:43043'], 
-  {
-    :buffer_requests => true,
-    :no_block => true,
-    :namespace => "benchmark_namespace"
-  }
-]
-
-@key = "key-"*8
-
-system("ruby #{HERE}/../setup.rb")
-sleep(1)
-
-@m = Memcached.new(*@opts)
-
-1000.times do
-  @m.set @key, @value
-  @m.set @key, @value
-  @m.set @key, @value
-  @m.get @key
-  @m.get @key
-  @m.get @key
-  @m.set @key, @value
-  @m.get @key
-  @m.set @key, @value
-  @m.get @key
-  @m.set @key, @value
-  @m.get @key
+class Worker  
+  def initialize
+    @key = "key-"*8  
+    @value = []
+    @marshalled = Marshal.dump(@value)
+    @opts = [
+      ['127.0.0.1:43042', '127.0.0.1:43043'], 
+      {
+        :buffer_requests => true,
+        :no_block => true,
+        :namespace => "benchmark_namespace"
+      }
+    ]    
+    system("ruby #{HERE}/../setup.rb")
+    sleep(1)  
+    @m = Memcached.new(*@opts)
+  end
+  
+  def work
+    @m.set @key, @value
+    @m.set @key, @value
+    @m.set @key, @value
+    @m.get @key
+    @m.get @key
+    @m.get @key
+    @m.set @key, @value
+    @m.get @key
+    @m.set @key, @value
+    @m.get @key
+    @m.set @key, @value
+    @m.get @key
+  end  
 end
+
+worker = Worker.new
+
+100.times do
+  worker.work
+end
+
+
