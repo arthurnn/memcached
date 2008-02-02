4c4ac85d9432b5d3909cb9335b844d1d3e2dd008
Evan Weaver <evan@cloudbur.st>
2008-02-02 09:26:25 +0000

    That todo has been did.

--- test/benchmark/valgrind.rb
+++ test/benchmark/valgrind.rb
@@ -1,4 +1,29 @@
 
+
+# At 2000 loops:
+#
+#==18554== LEAK SUMMARY:
+#==18554==    definitely lost: 34,128 bytes in 2 blocks.
+#==18554==    indirectly lost: 64 bytes in 1 blocks.
+#==18554==      possibly lost: 32 bytes in 1 blocks.
+#==18554==    still reachable: 764,457 bytes in 10,198 blocks.
+#==18554==         suppressed: 0 bytes in 0 blocks.
+#==18554== Reachable blocks (those to which a pointer was found) are not shown.
+#==18554== To see them, rerun with: --leak-check=full --show-reachable=yes
+#
+# At 5000 loops:
+#
+#==18693== LEAK SUMMARY:
+#==18693==    definitely lost: 34,128 bytes in 2 blocks.
+#==18693==    indirectly lost: 64 bytes in 1 blocks.
+#==18693==      possibly lost: 32 bytes in 1 blocks.
+#==18693==    still reachable: 764,457 bytes in 10,198 blocks.
+#==18693==         suppressed: 0 bytes in 0 blocks.
+#==18693== Reachable blocks (those to which a pointer was found) are not shown.
+#==18693== To see them, rerun with: --leak-check=full --show-reachable=yes
+#
+# Conclusion: no leaks.
+
 HERE = File.dirname(__FILE__)
 $LOAD_PATH << "#{HERE}/../../lib/"
 
