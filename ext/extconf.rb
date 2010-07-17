a4de102c1573b6c0354b65a9fb1c7255594a42f1
Rob Benson <robgbenson@gmail.com>
2010-07-17 07:14:38 +0000

    Crossport the get_len libmemcached patch over to fauna.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -67,6 +67,10 @@ def check_libmemcached
       puts(cmd = "#{patch} -p1 -Z < libmemcached-4.patch")
       raise "'#{cmd}' failed" unless system(cmd) or ENV['DEV']
 
+      puts "Patching libmemcached for get_len command."
+      puts(cmd = "#{patch} -p1 -Z < libmemcached-5.patch")
+      raise "'#{cmd}' failed" unless system(cmd) or ENV['DEV']
+
       puts "Touching aclocal.m4  in libmemcached."
       puts(cmd = "touch -r #{BUNDLE_PATH}/m4/visibility.m4 #{BUNDLE_PATH}/configure.ac #{BUNDLE_PATH}/m4/pandora_have_sasl.m4")
       raise "'#{cmd}' failed" unless system(cmd)
