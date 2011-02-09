2c9d7abe30712a42868df445e4b65b54353f0410
Evan Weaver <eweaver@twitter.com>
2011-02-08 16:53:30 -0800

    compile on BSD, theoretically

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -14,10 +14,20 @@ $LDFLAGS = "#{RbConfig::CONFIG['LDFLAGS']} #{$LDFLAGS} -L#{RbConfig::CONFIG['lib
 $CXXFLAGS = " -std=gnu++98 #{$CFLAGS}"
 $CPPFLAGS = $ARCH_FLAG = $DLDFLAGS = ""
 
+GMAKE_CMD = Config::CONFIG['host_os'].downcase =~ /bsd|solaris/ ? "gmake" : "make"
+TAR_CMD = SOLARIS_32 ? 'gtar' : 'tar'
+PATCH_CMD = SOLARIS_32 ? 'gpatch' : 'patch'
+
 if ENV['DEBUG']
   puts "Setting debug flags."
   $CFLAGS << " -O0 -ggdb -DHAVE_DEBUG"
   $EXTRA_CONF = ""
+end
+
+def patch(prefix, reason)
+  puts "Patching libmemcached source for #{reason}."
+  puts(cmd = "#{PATCH_CMD} -p1 -f < #{prefix}.patch")
+  raise "'#{cmd}' failed" unless system(cmd) or ENV['DEV']
 end
 
 def check_libmemcached
@@ -35,9 +45,6 @@ def check_libmemcached
     if File.exist?("lib")
       puts "Libmemcached already built; run 'rake clean' first if you need to rebuild."
     else
-      tar = SOLARIS_32 ? 'gtar' : 'tar'
-      patch = SOLARIS_32 ? 'gpatch' : 'patch'
-
       # have_sasl check may fail on OSX, skip it
       # unless RUBY_PLATFORM =~ /darwin/ or have_library('sasl2')
       #   raise "SASL2 not found. You need the libsasl2-dev library, which should be provided through your system's package manager."
@@ -46,36 +53,16 @@ def check_libmemcached
       system("rm -rf #{BUNDLE_PATH}") unless ENV['DEBUG'] or ENV['DEV']
 
       puts "Building libmemcached."
-      puts(cmd = "#{tar} xzf #{BUNDLE} 2>&1")
+      puts(cmd = "#{TAR_CMD} xzf #{BUNDLE} 2>&1")
       raise "'#{cmd}' failed" unless system(cmd)
 
-      puts "Patching libmemcached source."
-      puts(cmd = "#{patch} -p1 -Z < libmemcached.patch")
-      raise "'#{cmd}' failed" unless system(cmd) or ENV['DEV']
-
-      puts "Patching libmemcached with SASL support."
-      puts(cmd = "#{patch} -p1 -Z < sasl.patch")
-      raise "'#{cmd}' failed" unless system(cmd) or ENV['DEV']
-
-      puts "Patching libmemcached for get_from_last support."
-      puts(cmd = "#{patch} -p1 -Z < libmemcached-2.patch")
-      raise "'#{cmd}' failed" unless system(cmd) or ENV['DEV']
-
-      puts "Patching libmemcached for no block prepend and append support."
-      puts(cmd = "#{patch} -p1 -Z < libmemcached-3.patch")
-      raise "'#{cmd}' failed" unless system(cmd) or ENV['DEV']
-
-      puts "Patching libmemcached for noop hash support."
-      puts(cmd = "#{patch} -p1 -Z < libmemcached-4.patch")
-      raise "'#{cmd}' failed" unless system(cmd) or ENV['DEV']
-
-      puts "Patching libmemcached for get_len command."
-      puts(cmd = "#{patch} -p1 -Z < libmemcached-5.patch")
-      raise "'#{cmd}' failed" unless system(cmd) or ENV['DEV']
-
-      puts "Patching libmemcached for failure count breakage."
-      puts(cmd = "#{patch} -p1 -Z < libmemcached-6.patch")
-      raise "'#{cmd}' failed" unless system(cmd) or ENV['DEV']
+      patch("libmemcached", "mark-dead behavior")
+      patch("sasl", "SASL")
+      patch("libmemcached-2", "get_from_last method")
+      patch("libmemcached-3", "no block prepend and append")
+      patch("libmemcached-4", "noop hash")
+      patch("libmemcached-5", "get_len method")
+      patch("libmemcached-6", "failure count bug")
 
       puts "Touching aclocal.m4  in libmemcached."
       puts(cmd = "touch -r #{BUNDLE_PATH}/m4/visibility.m4 #{BUNDLE_PATH}/configure.ac #{BUNDLE_PATH}/m4/pandora_have_sasl.m4")
@@ -85,10 +72,10 @@ def check_libmemcached
         puts(cmd = "env CFLAGS='-fPIC #{$CFLAGS}' LDFLAGS='-fPIC #{$LDFLAGS}' ./configure --prefix=#{HERE} --without-memcached --disable-shared --disable-utils --disable-dependency-tracking #{$EXTRA_CONF} 2>&1")
         raise "'#{cmd}' failed" unless system(cmd)
 
-        puts(cmd = "make CXXFLAGS='#{$CXXFLAGS}' || true 2>&1")
+        puts(cmd = "#{GMAKE_CMD} CXXFLAGS='#{$CXXFLAGS}' || true 2>&1")
         raise "'#{cmd}' failed" unless system(cmd)
 
-        puts(cmd = "make install || true 2>&1")
+        puts(cmd = "#{GMAKE_CMD} install || true 2>&1")
         raise "'#{cmd}' failed" unless system(cmd)
       end
 
