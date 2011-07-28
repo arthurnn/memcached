d0ace27d60e0937282047b5adcbc5cc4e9bc5e6c
Sergey Avseyev <sergey.avseyev@gmail.com>
2011-07-28 12:38:38 +0300

    Allow using custom memcached executable for tests

--- test/setup.rb
+++ test/setup.rb
@@ -9,21 +9,22 @@ unless defined? UNIX_SOCKET_NAME
   # Start memcached
   verbosity = (ENV['DEBUG'] ? "-vv" : "")
   log = "/tmp/memcached.log"
+  memcached = ENV['MEMCACHED_COMMAND'] || 'memcached'
   system ">#{log}"
 
   # TCP memcached
   (43042..43046).each do |port|
-    cmd = "memcached #{verbosity} -U 0 -p #{port} >> #{log} 2>&1 &"
+    cmd = "#{memcached} #{verbosity} -U 0 -p #{port} >> #{log} 2>&1 &"
     raise "'#{cmd}' failed to start" unless system(cmd)
   end
   # UDP memcached
   (43052..43053).each do |port|
-    cmd = "memcached #{verbosity} -U #{port} -p 0 >> #{log} 2>&1 &"
+    cmd = "#{memcached} #{verbosity} -U #{port} -p 0 >> #{log} 2>&1 &"
     raise "'#{cmd}' failed to start" unless system(cmd)
   end
   # Domain socket memcached
   (0..1).each do |i|
-    cmd = "memcached -M -s #{UNIX_SOCKET_NAME}#{i} #{verbosity} >> #{log} 2>&1 &"
+    cmd = "#{memcached} -M -s #{UNIX_SOCKET_NAME}#{i} #{verbosity} >> #{log} 2>&1 &"
     raise "'#{cmd}' failed to start" unless system(cmd)
   end
 end
