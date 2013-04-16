87d12567acf2a6398ece9dd241c3718baf7d7257
Jason Gladish <jason@expectedbehavior.com>
2013-04-16 16:01:24 -0400

    Updated memcached to pass in keys and flags to the encoder.

--- lib/memcached/encoder/marshal.rb
+++ lib/memcached/encoder/marshal.rb
@@ -1,10 +1,10 @@
 class Memcached
   class Encoder
     class Marshal
-      def self.encode(value, flags)
+      def self.encode(key, value, flags)
         [::Marshal.dump(value), flags]
       end
-      def self.decode(value, flags)
+      def self.decode(key, value, flags)
         ::Marshal.load(value)
       end
     end
