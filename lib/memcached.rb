c2f4325b40118888371c3900407742b37546439a
Arthur Neves <arthurnn@gmail.com>
2016-03-04 11:12:51 -0500

    Start refactoring the main ruby class.

--- lib/memcached.rb
+++ lib/memcached.rb
@@ -1,33 +1,36 @@
+require 'taj'
 
-=begin rdoc
-The generated SWIG module for accessing libmemcached's C API.
-
-Includes the full set of libmemcached static methods (as defined in <tt>$INCLUDE_PATH/libmemcached/memcached.h</tt>), and classes for the available structs:
-
-* <b>Rlibmemcached::MemcachedResultSt</b>
-* <b>Rlibmemcached::MemcachedServerSt</b>
-* <b>Rlibmemcached::MemcachedSt</b>
-* <b>Rlibmemcached::MemcachedStatSt</b>
-* <b>Rlibmemcached::MemcachedStringSt</b>
-
-A number of SWIG typemaps and C helper methods are also defined in <tt>ext/libmemcached.i</tt>.
-
-=end
-module Rlibmemcached
+module Taj
+  class Connection
+    def self.create(_)
+      self.new
+    end
+  end
 end
 
-require 'rlibmemcached'
+module Memcached
+  class Client
 
-class Memcached
-  Lib = Rlibmemcached
-  raise "libmemcached 0.32 required; you somehow linked to #{Lib.memcached_lib_version}." unless "0.32" == Lib.memcached_lib_version
-  VERSION = File.read("#{File.dirname(__FILE__)}/../CHANGELOG")[/v([\d\.]+)\./, 1]
+    def initialize(servers)
+      @servers = servers
+    end
+
+    def set(key, value, ttl: @default_ttl, encode: true)#, flags: FLAGS)
+      connection.set(key, value.to_s)
+    end
+
+    def connection
+      @connection ||= Taj::Connection.create(@servers)
+    end
+  end
+#  raise "libmemcached 0.32 required; you somehow linked to #{Lib.memcached_lib_version}." unless "0.32" == Lib.memcached_lib_version
+#  VERSION = File.read("#{File.dirname(__FILE__)}/../CHANGELOG")[/v([\d\.]+)\./, 1]
 end
 
-require 'memcached/exceptions'
-require 'memcached/behaviors'
-require 'memcached/auth'
-require 'memcached/marshal_codec'
-require 'memcached/memcached'
-require 'memcached/rails'
-require 'memcached/experimental'
+#require 'memcached/exceptions'
+#require 'memcached/behaviors'
+#require 'memcached/auth'
+#require 'memcached/marshal_codec'
+#require 'memcached/memcached'
+#require 'memcached/rails'
+#require 'memcached/experimental'
