009cd64525ef9f924464e24b24dcff1a76f5c4db
Arthur Neves <arthurnn@gmail.com>
2016-03-04 13:40:22 -0500

    Add a Taj context struct

--- ext/taj/taj.c
+++ ext/taj/taj.c
@@ -2,16 +2,36 @@
 
 VALUE rb_mTaj;
 
+typedef struct {
+  memcached_st * memc;
+} Taj_ctx;
+
+static Taj_ctx *
+get_ctx(VALUE obj)
+{
+  Taj_ctx* ctx;
+  Data_Get_Struct(obj, Taj_ctx, ctx);
+  return ctx;
+}
+
+static void
+free_memc(Taj_ctx * ctx)
+{
+  memcached_free(ctx->memc);
+}
+
+
 static VALUE
 allocate(VALUE klass)
 {
-  memcached_st *memc;
-  const char *config_string = "--SERVER=localhost";
 
-  memc = memcached(config_string, strlen(config_string));
+  Taj_ctx * ctx;
+  VALUE rb_obj = Data_Make_Struct(klass, Taj_ctx, NULL, free_memc, ctx);
+
   // memc = memcached_create(NULL); TODO: Append servers
-
-  return Data_Wrap_Struct(klass, NULL, &memcached_free, memc);
+  const char *config_string = "--SERVER=localhost";
+  ctx->memc = memcached(config_string, strlen(config_string));
+  return rb_obj;
 }
 
 static VALUE
@@ -37,43 +57,40 @@ iterate_server_function(const memcached_st *ptr, const memcached_instance_st * s
 static VALUE
 rb_connection_servers(VALUE self)
 {
-  memcached_st *memc;
-  Data_Get_Struct(self, memcached_st, memc);
+  Taj_ctx *ctx = get_ctx(self);
 
   VALUE server_list = rb_ary_new();
 
   memcached_server_fn callbacks[1];
   callbacks[0] = iterate_server_function;
 
-  memcached_server_cursor(memc, callbacks, (void *)server_list, 1);
+  memcached_server_cursor(ctx->memc, callbacks, (void *)server_list, 1);
   return server_list;
 }
 
 static VALUE
 rb_connection_set(VALUE self, VALUE key, VALUE value)
 {
-  memcached_st *memc;
-  Data_Get_Struct(self, memcached_st, memc);
+  Taj_ctx *ctx = get_ctx(self);
 
   char *mkey = StringValuePtr(key);
   char *mvalue = StringValuePtr(value);
 
-  memcached_return_t rc = memcached_set(memc, mkey, strlen(mkey), mvalue, strlen(mvalue), (time_t)0, (uint32_t)0);
+  memcached_return_t rc = memcached_set(ctx->memc, mkey, strlen(mkey), mvalue, strlen(mvalue), (time_t)0, (uint32_t)0);
 
-  fprintf(stderr, "Couldn't store key: %s\n", memcached_strerror(memc, rc));
+  fprintf(stderr, "Couldn't store key: %s\n", memcached_strerror(ctx->memc, rc));
   return (rc == MEMCACHED_SUCCESS);
 }
 
 static VALUE
 rb_connection_get(VALUE self, VALUE key)
 {
-  memcached_st *memc;
-  Data_Get_Struct(self, memcached_st, memc);
+  Taj_ctx *ctx = get_ctx(self);
 
   char *mkey = StringValuePtr(key);
 
   size_t return_value_length;
-  char *response  = memcached_get(memc, mkey, strlen(mkey), &return_value_length, (time_t)0, (uint32_t)0);
+  char *response  = memcached_get(ctx->memc, mkey, strlen(mkey), &return_value_length, (time_t)0, (uint32_t)0);
 
   return rb_str_new2(response);
 }
