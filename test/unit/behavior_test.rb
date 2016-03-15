63fc043da5c652d25cc4f5568b115934461da15a
Arthur Neves <arthurnn@gmail.com>
2016-03-15 18:30:40 -0400

    Add more behavior tests

--- test/unit/behavior_test.rb
+++ test/unit/behavior_test.rb
@@ -12,4 +12,14 @@ class BehaviorTest < BaseTest
     cache.connection.set_behavior(Memcached::Behaviors::MEMCACHED_BEHAVIOR_HASH, Memcached::Behaviors::MEMCACHED_HASH_FNV1_64)
     assert_equal Memcached::Behaviors::MEMCACHED_HASH_FNV1_64, cache.connection.get_behavior(Memcached::Behaviors::MEMCACHED_BEHAVIOR_HASH)
   end
+
+  def test_setting_value_with_bool
+    cache.connection.set_behavior(Memcached::Behaviors::MEMCACHED_BEHAVIOR_USE_UDP, true)
+    assert cache.connection.get_behavior(Memcached::Behaviors::MEMCACHED_BEHAVIOR_USE_UDP)
+  end
+
+  def test_setting_value_with_invalid_num
+    cache.connection.set_behavior(Memcached::Behaviors::MEMCACHED_BEHAVIOR_HASH, 100)
+    assert_equal 0, cache.connection.get_behavior(Memcached::Behaviors::MEMCACHED_BEHAVIOR_HASH)
+  end
 end
