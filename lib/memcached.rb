f06a3cb3116e8d6b3d83350a47a16b8894aaf136
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2021-04-07 11:08:31 -0700

    Fix a circular require warning by autoloading optional modules
    The warnings was
    
    .../lib/memcached/rails.rb:1: warning: .../lib/memcached/rails.rb:1: warning: loading in progress, circular require considered harmful - .../lib/memcached.rb

--- lib/memcached.rb
+++ lib/memcached.rb
@@ -22,11 +22,12 @@ class Memcached
   Lib = Rlibmemcached
   raise "libmemcached 0.32 required; you somehow linked to #{Lib.memcached_lib_version}." unless "0.32" == Lib.memcached_lib_version
   VERSION = File.read("#{File.dirname(__FILE__)}/../CHANGELOG")[/v([\d\.]+)\./, 1]
+
+  autoload :Rails, 'memcached/rails'
+  autoload :Experimental, 'memcached/experimental'
 end
 
 require 'memcached/exceptions'
 require 'memcached/behaviors'
 require 'memcached/marshal_codec'
 require 'memcached/memcached'
-require 'memcached/rails'
-require 'memcached/experimental'
