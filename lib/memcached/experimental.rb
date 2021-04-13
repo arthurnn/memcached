0a1bccc08a917e6e926e2c8ab58021e0b0c8fd15
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2021-04-13 13:35:32 -0700

    Remove experimental get_len method
    The tests for this method were getting skipped because the server
    doesn't actually support this command.  I'm not sure when it was
    supported. Support for this was already removed from libmemcached.
    Since it was experimental in this library, we may as well just remove
    it now.

--- lib/memcached/experimental.rb
+++ lib/memcached/experimental.rb
@@ -1,6 +1,5 @@
 class Memcached
   module Experimental
-
     # TOUCH is used to set a new expiration time for an existing item
     def touch(key, ttl=@default_ttl)
       check_return_code(
@@ -13,36 +12,5 @@ class Memcached
       tries += 1
       retry
     end
-
-    def get_len(bytes, keys)
-      if keys.is_a? ::Array
-        # Multi get
-        ret = Lib.memcached_mget_len(@struct, keys, bytes);
-        check_return_code(ret, keys)
-
-        hash = {}
-        keys.each do
-          value, key, _flags, ret = Lib.memcached_fetch_rvalue(@struct)
-          break if ret == Lib::MEMCACHED_END
-          if ret != Lib::MEMCACHED_NOTFOUND
-            check_return_code(ret, key)
-            # Assign the value
-            hash[key] = value
-          end
-        end
-        hash
-      else
-        # Single get_len
-        value, _flags, ret = Lib.memcached_get_len_rvalue(@struct, keys, bytes)
-        check_return_code(ret, keys)
-        value
-      end
-    rescue => e
-      tries ||= 0
-      raise unless tries < options[:exception_retry_limit] && should_retry(e)
-      tries += 1
-      retry
-    end
-
   end
 end
